31df5bb8453220aa89e5784aeebcbc47b421a326
Hazel Llorente <160571845+hazziebaezzy@users.noreply.github.com>
2024-09-20 22:55:01 +0800

    Add files via upload

---
+++ qgis/layers/layers.js
@@ -0,0 +1,41 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleRoad_0 = new ol.layer.Tile({
+            'title': 'Google Road',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+            })
+        });
+
+        var lyr_OSMStandard_1 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_OpenStreetMap_2 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+lyr_GoogleRoad_0.setVisible(true);lyr_OSMStandard_1.setVisible(true);lyr_OpenStreetMap_2.setVisible(true);
+var layersList = [lyr_GoogleRoad_0,lyr_OSMStandard_1,lyr_OpenStreetMap_2];
